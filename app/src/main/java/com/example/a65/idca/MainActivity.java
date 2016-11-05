2d0f75ffd9f84ee22bf7b53e885acc699e0f8b5f
dominikyeyo <diegoferneynunez@gmail.com>
2016-11-05 13:04:56 -0500

    AndroidA

--- app/src/main/java/com/example/a65/idca/MainActivity.java
+++ app/src/main/java/com/example/a65/idca/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.a65.idca;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName() ;
+
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +28,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        textViewResult = (TextView) findViewById(R.id.textViewResult);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+       //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.btn_calcular);
+
     }
 
-    @Override
+    public void calcularIMC(View view) {
+
+        Log.d(TAG, "Entra a calcular IMC con el click");
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        EditText editTextAltura = (EditText) findViewById(R.id.editTextCentimetros);
+        EditText editTextPeso = (EditText) findViewById(R.id.editTextCentimetros);
+
+        String altura = editTextAltura.getText().toString();
+        String peso = editTextPeso.getText().toString();
+
+        if(altura.isEmpty() && peso.isEmpty()) return;
+        if(altura.matches("\\d+") && peso.matches("\\d+")){
+
+            float cm = Integer.valueOf(altura);
+            float kg = Integer.valueOf(peso);
+
+            calcular(cm, kg);
+
+
+        }
+
+    }
+
+    private void calcular(float cm, float kg ){
+
+        cm = (cm/100);
+        float bmi = kg / (cm * cm);
+
+        String resultado;
+
+        if(bmi < 16){
+            resultado = "Tu BMI" + bmi + "( severamente bajo )";
+        }else if(bmi < 18.5){
+            resultado = "Tu BMI" + bmi + "( bajo )";
+        }else if(bmi < 25){
+            resultado = "Tu BMI" + bmi + "( normal )";
+        }else if(bmi < 30){
+            resultado = "Tu BMI" + bmi + "( sobrepeso )";
+        }else {
+            resultado = "Tu BMI" + bmi + "( Obeso )";
+        }
+
+        Log.i(TAG,"resultado" + resultado);
+
+        textViewResult.setText( resultado
+        );
+
+
+    }
+
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -48,5 +105,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
